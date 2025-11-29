a5ffc65ecf8a535ab3b3bb30290dbc3070cd3444
Y7000P <2522763377@qq.com>
2025-11-29 15:32:58 +0800

    finish hw01

--- HW/hw01/hw01.py
+++ HW/hw01/hw01.py
@@ -13,9 +13,9 @@ def a_plus_abs_b(a, b):
     3
     """
     if b < 0:
-        f = _____
+        f = lambda x, y : x - y
     else:
-        f = _____
+        f = lambda x, y : x + y
     return f(a, b)
 
 def a_plus_abs_b_syntax_check():
@@ -42,7 +42,8 @@ def two_of_three(i, j, k):
     >>> two_of_three(5, 5, 5)
     50
     """
-    return _____
+
+    return i * i + j * j + k * k - max(i, j, k) * max(i, j, k)
 
 def two_of_three_syntax_check():
     """Check that your two_of_three code consists of nothing but a return statement.
@@ -66,6 +67,13 @@ def largest_factor(n):
     1
     """
     "*** YOUR CODE HERE ***"
+    res = n
+
+    for i in range(1, n):
+        if n % i == 0 and i != n:
+            res = i
+
+    return res
 
 
 def hailstone(n):
@@ -88,4 +96,15 @@ def hailstone(n):
     1
     """
     "*** YOUR CODE HERE ***"
+    length = 1
+    print(n)
+    while n != 1 :
+        if n % 2 != 0:
+            n = n * 3 + 1
+        else :
+            n //= 2
+        print(n)
+        length += 1
+
+    return length
 
